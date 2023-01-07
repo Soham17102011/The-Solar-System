b484930590f3f765c4f773884d7ce1299ef28f05
Soham17102011 <115869174+Soham17102011@users.noreply.github.com>
2023-01-07 12:28:06 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function pluto() {
+    document.getElementById("info").innerHTML = "•	Pluto is the 9th planet in the Solar System.             " +
+        "•It is 4.6 billion years old."
+    "•It is a rocky planet." +
+    "•Its distance from Sun is 5.9 billion kilometers." +
+    "•Pluto 's temperature is -387°F (-232°C)•" +
+    "On August 24, 2006 International Astronomical Union(IAU) removed its status from a planet to a dwarf planet." +
+    "•It takes Pluto 268 years to complete a full orbit around the Sun." +
+    "•Diameter of“ Pluto” is 2,376.6 kilometers, makes it the smallest planet in the Solar System." +
+    "•It has 4 moons."
+
+
+
+}
